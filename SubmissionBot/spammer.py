231eee7dca16ccbf521716b25b8782a425199b9d
mango <adamtamers@outlook.com>
2020-12-08 00:57:58 +0400

    Form Spammer
    Does what the name says yall

---
+++ SubmissionBot/spammer.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import random
+import string
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+url = 'FORM LINK'
+
+names = json.loads(open('names.json').read())
+
+for name in names:
+	name_extra = ''.join(random.choice(string.digits))
+
+	email = name.lower() + name_extra + '@gmail'
+	discord = ''.join(random.choice(chars) for i in range(8))
+	message = ''.join(random.choice(chars) for i in range(8))
+        
+	requests.post(url, allow_redirects=False, data={
+                'email@email.com': email,
+                'discord': name,
+		'message': message,
+	})
